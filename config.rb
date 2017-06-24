d371f37590ffe52c28243ef09672d6ca251d8fe8
Travis-CI <yermilov17@gmail.com>
2017-06-24 23:40:58 +0000

    deployed build-4 by Travis-CI from 1c12a4420f85676a2b8bba271ac155c089d67d2b (specify gh_pages url)

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/home/travis/build/yermilov/grain-example/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/home/travis/build/yermilov/grain-example/theme/sass"
+generated_images_path = "/home/travis/build/yermilov/grain-example/.cache/compass/images"
+images_path = "/home/travis/build/yermilov/grain-example/theme/images"
+fonts_path = "/home/travis/build/yermilov/grain-example/theme/fonts"
+
+line_comments = false
+output_style = :compressed
